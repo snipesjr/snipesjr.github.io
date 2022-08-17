44d468fd29f7d4a00d34f5c6d0321580acb1af0f
snipesjr <JakeSnipes@outlook.com>
2022-08-17 17:58:47 +0000

    Deploying to public from @ snipesjr/snipesjr.github.io@87049f8b9e394eade952c07eca8bb741b2d8cfb9 🚀

---
+++ _next/static/oMK10Nv8bmcWT4MH3PcrJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/55b5f309bfec9441.css","static/chunks/pages/index-832fe3a843d60b2d.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()

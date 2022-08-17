87049f8b9e394eade952c07eca8bb741b2d8cfb9
Jake Snipes <jakesnipes@outlook.com>
2022-08-17 13:57:55 -0400

    Refactor icons into components dir

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Jake Snipes
+
+
+# Reference projects
++ [Next.js example blog](https://github.com/vercel/next.js/tree/canary/examples/blog-starter)
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from "next/link";
 
-import { GithubIcon, LinkedInIcon} from "../components/icons";
+import { GithubIcon, LinkedInIcon} from '../components/icons';
 
 
 const Home: NextPage = () => {

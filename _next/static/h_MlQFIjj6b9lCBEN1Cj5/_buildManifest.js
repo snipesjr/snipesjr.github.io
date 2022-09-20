819444a7a32a774719195cff6c6fb69ff3cceffe
snipesjr <JakeSnipes@outlook.com>
2022-09-20 17:26:49 +0000

    Deploying to public from @ snipesjr/snipesjr.github.io@0394ee9b6a65245ab8ff92db9d01b433ed2e04fa 🚀

---
+++ _next/static/h_MlQFIjj6b9lCBEN1Cj5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-a835f7ae8ab936f5.js","static/chunks/pages/index-020648f7d8ca9e49.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
